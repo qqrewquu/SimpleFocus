f31002c3488e598caf31e07c07fadb3e09a0f74d
qqrewquu <zifeng.guo09@outlook.com>
2025-10-17 15:42:38 -0400

    Initial Commit

---
+++ SimpleFocus/SimpleFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFocusApp.swift
+//  SimpleFocus
+//
+//  Created by Zifeng Guo on 2025-10-17.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
